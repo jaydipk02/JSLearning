510036cc38c08acb053f73a614530ded9488096d
jaydipk02 <144699105+jaydipk02@users.noreply.github.com>
2023-09-20 17:17:28 +0530

    variable assignment
    variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+
+
+console.log("Hello");
+var myFullName,firstName="Jaydip",
+lastName="kashid",
+myName="jaydip kashid",
+myName="jaydip vasant kashid",
+myLove="I love only JS";
+console.log(myFullName);
+console.log("my name-",myName);
+console.log("my full name-",myName);
+console.log(myLove);
+console.log("first name-",firstName);
+console.log("Last name-",lastName);
+var age="25"
+console.log("age-",age);
+var address="PCMC pune"
+console.log("address-",address);
+var pinCode="411018";
+console.log("Pincode",pinCode);
+var gender="male"
+console.log("gender-",gender);
+var isMarried="No"
+console.log("martial status-",isMarried);
---
+++ 01_varibal.js
@@ -0,0 +1,14 @@
+console.log("Hello");//direct print any thing use of "" 
+var Jay;
+console.log(Jay);
+var lastName,firstName ,height,address,marks; //variable declaration
+firstName="Jaydip"   //value assign to variables
+console.log("Name-",firstName);//variable access the value
+lastName="Kashid"
+console.log("Last name-",lastName);
+height="5.9"
+console.log(" Height-",height);
+address="pune"
+console.log ("Address-",address);
+marks="75%";
+console.log("10th Marks-",marks);
